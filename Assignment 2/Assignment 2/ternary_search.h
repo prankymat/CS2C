87a9e4764600663cdc2aa5e8ea6e87926264a502
Matthew Wo <9029537@gmail.com>
2016-01-11 18:53:32 -0800

    no error, possible bug in algorithm.

--- Assignment 2/Assignment 2/ternary_search.h
+++ Assignment 2/Assignment 2/ternary_search.h
@@ -1,10 +1,26 @@
 template <typename ArrayLike, typename T>
 int ternary_search(const ArrayLike& array, const T& value, int low, int high) {
-   // Replace the code below with code that actually does a tenary search.
-   for (int i = low; i < high; ++i) {
-      if (array[i] == value) {
-         return i;
-      }
+
+   int left = low + (high - low) / 3;
+   int right = low + (high - low) * 2 / 3;
+   
+   if (value == array[left]) {
+      return left;
+   } else if (value == array[right]) {
+      return right;
    }
+
+   if (left == right) {
+      return -1;
+   }
+
+   if (value > array[left] && value < array[right]) {
+      return ternary_search(array, value, left, right);
+   } else if (value < array[left]) {
+      return ternary_search(array, value, low, left);
+   } else if (value > array[right]) {
+      return ternary_search(array, value, right, high);
+   }
+
    return -1;
 }
