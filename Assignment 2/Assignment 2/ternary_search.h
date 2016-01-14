9c56b1c668b9ab9830647fc5ed3d6db57efbb2f1
Matthew Wo <9029537@gmail.com>
2016-01-13 20:25:36 -0800

    100% working algorithm :)

--- Assignment 2/Assignment 2/ternary_search.h
+++ Assignment 2/Assignment 2/ternary_search.h
@@ -1,20 +1,29 @@
 template <typename ArrayLike, typename T>
 int ternary_search(const ArrayLike& array, const T& value, int low, int high) {
-   if (low > high) {
+   if (low == high) {
       return -1;
    }
 
-   int left = low + (high - low) / 3;
-   int right = low + 2 * (high - low) / 3;
+   int third = (high - low) / 3;
+   int left = low + third;
+   int right = low + 2 * third;
 
    if (value <= array[left]) {
       // Left pile
-      return (value == array[left] ? left : ternary_search(array, value, low, left-1));
+      if (value == array[left]) {
+         // Found at left
+         return left;
+      }
+      return ternary_search(array, value, low, left);
    } else if (value >= array[right]) {
       // Right pile
-      return (value == array[right] ? right : ternary_search(array, value, right+1, high));
+      if (value == array[right]) {
+         // Found at right
+         return right;
+      }
+      return ternary_search(array, value, right+1, high);
    } else {
       // Middle pile
-      return ternary_search(array, value, left, right);
+      return ternary_search(array, value, left+1, right);
    }
 }
