1a7e984c1f84e1ad38e245c78245dd5fb5451f1d
Matthew Wo <9029537@gmail.com>
2016-01-13 17:45:14 -0800

    99% score. Will access out of bound.

--- Assignment 2/Assignment 2/ternary_search.h
+++ Assignment 2/Assignment 2/ternary_search.h
@@ -1,26 +1,20 @@
 template <typename ArrayLike, typename T>
 int ternary_search(const ArrayLike& array, const T& value, int low, int high) {
-
-   int left = low + (high - low) / 3;
-   int right = low + (high - low) * 2 / 3;
-   
-   if (value == array[left]) {
-      return left;
-   } else if (value == array[right]) {
-      return right;
-   }
-
-   if (left == right) {
+   if (low > high) {
       return -1;
    }
 
-   if (value > array[left] && value < array[right]) {
-      return ternary_search(array, value, left, right);
-   } else if (value < array[left]) {
-      return ternary_search(array, value, low, left);
-   } else if (value > array[right]) {
-      return ternary_search(array, value, right, high);
-   }
+   int left = low + (high - low) / 3;
+   int right = low + 2 * (high - low) / 3;
 
-   return -1;
+   if (value <= array[left]) {
+      // Left pile
+      return (value == array[left] ? left : ternary_search(array, value, low, left-1));
+   } else if (value >= array[right]) {
+      // Right pile
+      return (value == array[right] ? right : ternary_search(array, value, right+1, high));
+   } else {
+      // Middle pile
+      return ternary_search(array, value, left, right);
+   }
 }
