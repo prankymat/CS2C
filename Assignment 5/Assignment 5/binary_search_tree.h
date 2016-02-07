ef137abdf96495e7ce40d4b76dfe214be99d0e44
Matthew Wo <9029537@gmail.com>
2016-02-07 09:17:01 -0800

    Removed unused codes and comments

--- Assignment 5/Assignment 5/binary_search_tree.h
+++ Assignment 5/Assignment 5/binary_search_tree.h
@@ -1,11 +1,3 @@
-//
-//  binary_search_tree.h
-//  Assignment 5
-//
-//  Created by Matthew Wo on 2/1/16.
-//  Copyright © 2016 Matthew Wo. All rights reserved.
-//
-
 #ifndef binary_search_tree_h
 #define binary_search_tree_h
 
